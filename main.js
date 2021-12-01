d97c5065d1d6db513852fa133fdd531cb2ff199a
margrayengine <93706595+margrayengine@users.noreply.github.com>
2021-12-01 11:28:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,91 @@ function openDownloads() {
     window.location.assign("downloads.html")
 }
 
+function getFromURL(param){
+    const query = window.location.search;
+    const urlParams = new URLSearchParams(query);
+    if(urlParams.has(param)){
+        return urlParams.get(param)
+    }    
+    else{
+        return ""
+    }
+}
+
+function getTheme(){
+    return getFromURL("theme")
+}
+function setTheme(){
+    if (getTheme() == "dark") {
+        document.body.classList.add("dark-mode");
+        document.getElementById("head").style.color = "rgb(255, 90, 49)"
+    } else {
+        if(document.body.classList.value.includes("dark-mode")) {
+          document.body.classList.remove("dark-mode");
+          
+        }
+        
+    }
+}
+function hasTheme(){
+    const query = window.location.search;
+    const urlParams = new URLSearchParams(query);
+    if(urlParams.has('theme')){
+        return true
+    }
+    else{
+        return false
+    }
+}
+function isTheme(theme){
+    has = hasTheme()
+    if(has){
+        if(getTheme() === theme){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    else{
+        return false;
+    }
+}
+function setParam(param,value){
+    var searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(param,value)
+    return searchParams
+}
+function nwt(url="",toId=null,s=null){
+    if(s === null){
+        var theme
+        if(document.body.classList.value.includes("dark-mode")){
+            theme = "dark"
+        }
+        else{
+            theme = "light"
+        }
+        if(toId!==null)window.location.assign(url+`?theme=${theme}#${toId}`)
+        else window.location.assign(url+`?theme=${theme}`)
+    }
+    else{
+        if(toId!==null)window.location.assign(url+`?theme=${s}#${toId}`)
+        else window.location.assign(url+`?theme=${s}`)
+    }
+}
+function localTheme(){
+    var theme
+    if(document.body.classList.value.includes("dark-mode")){
+        theme = "dark"
+    }
+    else{
+        theme = "light"
+    }
+    return theme
+}
+function navindocs(id){
+    window.location.assign(`?theme=${localTheme()}#${id}`)
+}
 function openHome() {
     window.location.assign("home.html")
 }
@@ -26,27 +111,60 @@ function openExample() {
 function openSideNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-
-    //document.getElementById("heading").style.marginLeft = "320px";
-    //document.getElementById("head").style.marginLeft = "320px";
 }
 function closeSideNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-    document.getElementById("heading").style.marginLeft = "0";
-    document.getElementById("head").style.marginLeft = "0";
 }
 
 window.onload = function() {
     scrollFunction()
+
+}
+function giveWhite(){
+    let heads = document.getElementsByClassName("sheading")
+    for(var i=0;heads.length>i;i++){
+        heads[i].style.color = "white"
+    }
+    document.getElementById("sheadhome").style.color = "white"
+    let homeA = document.getElementsByClassName("mgp")
+    if(homeA.length > 0){
+        for(var i=0;homeA.length>i;i++){
+            homeA[i].style.color = "white"
+        }       
+    }
+}
+function giveBlack(){
+    let heads = document.getElementsByClassName("sheading")
+    for(var i=0;heads.length>i;i++){
+        heads[i].style.color = "black"
+    }
+    document.getElementById("sheadhome").style.color = "black"
+    let homeA = document.getElementsByClassName("mgp")
+    if(homeA.length > 0){
+        for(var i=0;homeA.length>i;i++){
+            homeA[i].style.color = "black"
+        }       
+    }
 }
 window.onscroll = function() {
     scrollFunction();
-    console.log(document.body.scrollTop)
 }
 function toggleTheme(){
     document.body.classList.toggle("dark-mode")
+    if(document.body.classList.value === "dark-mode"){
+        document.getElementById("heading").style.color = "rgb(255, 99, 49)"
+        giveWhite()
+        //window.location.assign("?"+setParam("theme","dark"))
+    }
+    else{
+        document.getElementById("heading").style.color = "rgb(255, 59, 45)"
+        //window.location.assign("?"+setParam("theme","light"))
+        giveBlack()
+    }
+
 }
+
 function scrollFunction() {
     var up_scroller = document.getElementById("up-scroller");
     var down_scroller = document.getElementById("down-scroller");
@@ -137,18 +255,4 @@ function dwnlinux() {
     download("margray installer.zip", "margray installer win32.zip")
     var modal = document.getElementById("Modal");
     modal.style.display = "none";
-}
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "320px";
-    document.getElementById("main").style.marginLeft = "320px";
-    document.getElementById("heading").style.marginLeft = "320px";
-    document.body.style.background = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),url('/image.jpg')";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.getElementById("heading").style.marginLeft = "0";
-    document.body.style.background = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),url('/image.jpg')";
 }
